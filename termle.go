ee02ff860615a5d1fc7792f445fdebd727725dd8
Jeremy Loy <jeremy.b.loy@icloud.com>
2022-01-21 00:13:55 -0500

    clean up code

--- termle.go
+++ termle.go
@@ -13,126 +13,19 @@ import (
 	"time"
 )
 
-func green(l string) string {
-	return "\033[37;102m" + l + "\033[0m"
-}
-func yellow(l string) string {
-	return "\033[37;103m" + l + "\033[0m"
-}
-func white(l string) string {
-	return "\033[0;107m" + l + "\033[0m"
-}
-func black(l string) string {
-	return "\033[37;100m" + l + "\033[0m"
-}
-func clearBoard() {
-	fmt.Print("\033c")
-}
+const totalWordles = 2315
 
-func prompt() {
-	fmt.Print(">")
-}
+var (
+	//go:embed guesses.txt
+	//go:embed answers.txt
+	files embed.FS
 
-type board [][]string
+	dayFlag    = flag.Int("day", currentDay(), "select a specific wordle by day")
+	randomFlag = flag.Bool("random", false, "pick a random wordle")
 
-func (b board) addGuess(turn int, guess, answer string) {
-	for i, c := range guess {
-		if c == rune(answer[i]) {
-			b[turn][i] = green(string(c))
-		} else if strings.ContainsRune(answer, c) {
-			b[turn][i] = yellow(string(c))
-		} else {
-			b[turn][i] = black(string(c))
-		}
-	}
-}
-
-func (b board) print() {
-	for _, y := range b {
-		for _, x := range y {
-			fmt.Print(" " + x)
-		}
-		fmt.Println()
-	}
-}
-
-func newBoard() board {
-	b := make(board, 6)
-	for i := range b {
-		b[i] = make([]string, 5)
-		for j := range b[i] {
-			b[i][j] = black("_")
-		}
-	}
-	return b
-}
-
-func printTurn(b board) {
-	clearBoard()
-	b.print()
-	prompt()
-}
-
-func printTurnWithError(b board, err string) {
-	clearBoard()
-	b.print()
-	fmt.Println(err)
-	prompt()
-}
-
-func parseGuesses() map[string]struct{} {
-	guessesFile, err := files.Open("guesses.txt")
-	if err != nil {
-		panic(err)
-	}
-	validGuesses := make(map[string]struct{})
-	guessesReader := bufio.NewScanner(guessesFile)
-
-	for guessesReader.Scan() {
-		validGuesses[strings.ToUpper(guessesReader.Text())] = struct{}{}
-	}
-	if guessesReader.Err() != nil {
-		panic(err)
-	}
-	return validGuesses
-}
-
-func currentAnswer(day int) string {
-	answersFile, err := files.Open("answers.txt")
-	if err != nil {
-		panic(err)
-	}
-	seeker := answersFile.(io.ReadSeeker)
-	_, err = seeker.Seek(int64(day*7), io.SeekStart)
-	if err != nil {
-		panic(err)
-	}
-	answer := make([]byte, 5)
-	_, err = seeker.Read(answer)
-	if err != nil {
-		panic(err)
-	}
-	return strings.ToUpper(string(answer))
-}
-
-var firstDay = time.Date(2021, time.June, 19, 0, 0, 0, 0, time.UTC)
-
-func currentDay() int {
-	return int(time.Now().UTC().Sub(firstDay).Hours()/24) - 1
-}
-
-var valid = regexp.MustCompile(`^[A-Za-z]{5}$`)
-
-//go:embed guesses.txt
-//go:embed answers.txt
-var files embed.FS
-
-var dayFlag = flag.Int("day", currentDay(), "select a specific wordle by day")
-var randomFlag = flag.Bool("random", false, "pick a random wordle")
-
-func randomDay() int {
-	return rand.Intn(2315)
-}
+	firstDay = time.Date(2021, time.June, 19, 0, 0, 0, 0, time.UTC)
+	valid    = regexp.MustCompile(`^[A-Za-z]{5}$`)
+)
 
 func main() {
 	flag.Parse()
@@ -144,8 +37,8 @@ func main() {
 		day = *dayFlag
 	}
 
-	answer := currentAnswer(day)
-	validGuesses := parseGuesses()
+	answer := answerForDay(day)
+	validGuesses := guessesSet()
 	b := newBoard()
 	currentTurn := 0
 	turnsRemaining := 6
@@ -180,4 +73,113 @@ func main() {
 	if s.Err() != nil {
 		panic(s.Err())
 	}
+}
+
+func green(l string) string {
+	return "\033[37;102m" + l + "\033[0m"
+}
+func yellow(l string) string {
+	return "\033[37;103m" + l + "\033[0m"
+}
+func white(l string) string {
+	return "\033[0;107m" + l + "\033[0m"
+}
+func black(l string) string {
+	return "\033[37;100m" + l + "\033[0m"
+}
+func clearBoard() {
+	fmt.Print("\033c")
+}
+func prompt() {
+	fmt.Print(">")
+}
+
+type board [][]string
+
+func newBoard() board {
+	b := make(board, 6)
+	for i := range b {
+		b[i] = make([]string, 5)
+		for j := range b[i] {
+			b[i][j] = black("_")
+		}
+	}
+	return b
+}
+
+func (b board) addGuess(turn int, guess, answer string) {
+	for i, c := range guess {
+		if c == rune(answer[i]) {
+			b[turn][i] = green(string(c))
+		} else if strings.ContainsRune(answer, c) {
+			b[turn][i] = yellow(string(c))
+		} else {
+			b[turn][i] = black(string(c))
+		}
+	}
+}
+
+func (b board) print() {
+	for _, y := range b {
+		for _, x := range y {
+			fmt.Print(" " + x)
+		}
+		fmt.Println()
+	}
+}
+
+func printTurn(b board) {
+	clearBoard()
+	b.print()
+	prompt()
+}
+
+func printTurnWithError(b board, err string) {
+	clearBoard()
+	b.print()
+	fmt.Println(err)
+	prompt()
+}
+
+func guessesSet() map[string]struct{} {
+	guessesFile, err := files.Open("guesses.txt")
+	if err != nil {
+		panic(err)
+	}
+	validGuesses := make(map[string]struct{})
+	guessesReader := bufio.NewScanner(guessesFile)
+
+	for guessesReader.Scan() {
+		validGuesses[strings.ToUpper(guessesReader.Text())] = struct{}{}
+	}
+	if guessesReader.Err() != nil {
+		panic(err)
+	}
+	return validGuesses
+}
+
+func answerForDay(day int) string {
+	answersFile, err := files.Open("answers.txt")
+	if err != nil {
+		panic(err)
+	}
+	seeker := answersFile.(io.ReadSeeker)
+	_, err = seeker.Seek(int64(day*7), io.SeekStart)
+	if err != nil {
+		panic(err)
+	}
+	answer := make([]byte, 5)
+	_, err = seeker.Read(answer)
+	if err != nil {
+		panic(err)
+	}
+	return strings.ToUpper(string(answer))
+}
+
+func currentDay() int {
+	return int(time.Now().UTC().Sub(firstDay).Hours()/24) - 1
+}
+
+func randomDay() int {
+	return rand.Intn(totalWordles)
 }
